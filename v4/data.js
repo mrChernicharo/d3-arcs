38d39976e551ac284e2c898e7e5fb2edcc5a3f25
Felipe Chernicharo <string7dev@gmail.com>
2021-12-07 11:22:43 -0500

    best2 the ultimate pie

---
+++ v4/data.js
@@ -0,0 +1,69 @@
+//both female and male data
+var totals = [
+	{
+		title: 'Soft-serve',
+		value: 686,
+		all: 1098,
+	},
+	{
+		title: 'Scooped',
+		value: 472,
+		all: 1098,
+	},
+	{
+		title: 'No Preference',
+		value: 318,
+		all: 1098,
+	},
+	{
+		title: 'Not Sure',
+		value: 22,
+		all: 1098,
+	},
+];
+//female
+var femaleData = [
+	{
+		title: 'Soft-serve',
+		value: 25,
+		all: 100,
+	},
+	{
+		title: 'Scooped',
+		value: 44,
+		all: 100,
+	},
+	{
+		title: 'No Preference',
+		value: 28,
+		all: 100,
+	},
+	{
+		title: 'Not Sure',
+		value: 3,
+		all: 100,
+	},
+];
+//male
+var maleData = [
+	{
+		title: 'Soft-serve',
+		value: 27,
+		all: 100,
+	},
+	{
+		title: 'Scooped',
+		value: 42,
+		all: 100,
+	},
+	{
+		title: 'No Preference',
+		value: 30,
+		all: 100,
+	},
+	{
+		title: 'Not Sure',
+		value: 2,
+		all: 100,
+	},
+];
